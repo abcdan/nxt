92b4e69ad2f2fe820f5f9dd3c0b8efeca276bf89
abcdan <daniel@lngzl.nl>
2023-12-25 17:07:27 +0100

    ✨ Added some preclaimed redirects

--- src/routes/redirect.go
+++ src/routes/redirect.go
@@ -8,7 +8,25 @@ import (
 
 func RedirectRoutes(app *fiber.App) {
 	app.Get("/:shortCode", func(c *fiber.Ctx) error {
+	
 		shortCode := c.Params("shortCode")
+		
+		preClaimed := map[string]string{
+			"source":     "https://github.com/abcdan/nxt",
+			"sourcecode": "https://github.com/abcdan/nxt",
+			"code":       "https://github.com/abcdan/nxt",
+			"github":     "https://github.com/abcdan/nxt",
+			"src":        "https://github.com/abcdan/nxt",
+			"deploy":     "https://railway.app/template/gSvwgO?referralCode=QsZ-bg",
+			"railway":    "https://railway.app/template/gSvwgO?referralCode=QsZ-bg",
+			"host":       "https://railway.app/template/gSvwgO?referralCode=QsZ-bg",
+			"up":         "https://railway.app/template/gSvwgO?referralCode=QsZ-bg",
+		}
+
+		if url, ok := preClaimed[shortCode]; ok {
+			return c.Redirect(url, fiber.StatusFound)
+		}
+
 		link, err := helper.GetLinkByShortcode(shortCode)
 		if err != nil {
 			return c.Status(fiber.StatusNotFound).SendFile("./public/404.html")
@@ -16,4 +34,5 @@ func RedirectRoutes(app *fiber.App) {
 		return c.Redirect(link.URL, fiber.StatusFound)
 	})
 }
+
 
